e40cd47c0de65744855979760fd4dbcb07bc4e79
adamlinc <adamlinc@gmail.com>
2019-11-25 15:24:57 -0300

    Update Consolidação de varios CSVs em um unico arquivo CSV.py

--- Consolidação de varios CSVs em um unico arquivo CSV.py
+++ Consolidação de varios CSVs em um unico arquivo CSV.py
@@ -9,19 +9,21 @@ import os
 import pandas as pd
 
 
+
 ''' Abre o diretório atual onde estão os arquivos'''
-path = os.getcwd() 
+path = os.getcwd()
+#path = os.chdir('C:\\Users\\ADAMLINCOLNOLIVEIRAS\\Box Sync\\Diretorio_Camara_de_Retencao')
 
 
 ''' Guarda os arquivos em variável '''
 files = os.listdir(path)
 
 ''' Abaixo faz um for em todos os arquivos do diretório para verificar se a extensão é CSV
-(-3 significa últimos 3 caracteres do nome do arquivo, ou seja, extensão). 
+(-3 significa últimos 3 caracteres do nome do arquivo, ou seja, extensão).
 Estas condições em python (For e If por exemplo) em uma única linha se chama list
 Comprehension '''
 
-files_csv = [file for file in files if file[-3:] == "csv"]
+files_csv = [file for file in files if file[-3:] == "TXT"]
 
 ''' Cria dataframe vazio '''
 df = pd.DataFrame()
@@ -31,8 +33,8 @@ df = pd.DataFrame()
 no passo anterior. No caso de csv, é necessário colocar o parâmetro sep=";" que é o separador '''
 
 for file in files_csv:
-    data = pd.read_csv(file, sep=";", encoding='iso-8859-1')
+    data = pd.read_csv(file, sep=";", names=["SITE", "UF", "DATA CONTA", "CICLO","QTE CONTAS","VALOR", "TIPO", "SUB TIPO"], encoding='iso-8859-1', index_col=False) #iso-8859-1
     df = df.append(data)
-    
-''' Salva o arquivo com nome VEIC_consolidado.csv no mesmo diretorio '''    
-df.to_csv(path + r'\VEIC_consolidado.csv' ,  sep=";", index=False)
+
+
+df.to_csv('C:\\Users\\ADAMLINCOLNOLIVEIRAS\\Box Sync\\Diretorio_Camara_de_Retencao\\Nova pasta\\20191122_Consolidados_camara_de_retencao.csv', sep = ';', index=False)  #atualiação - caminho path+ nome agora é separado por virgula e SEP=
